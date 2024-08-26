1eb880140a4891df324d85dc691825beb02cafe4
Jo Wagner <jowagner@limago.de>
2024-08-26 09:48:25 +0200

    initial commit

--- Tag1_01Decorator/src/main/Main.java
+++ Tag1_01Decorator/src/main/Main.java
@@ -3,8 +3,18 @@ package main;
 import client.CalcClient;
 import math.*;
 
+import java.time.Duration;
+import java.time.Instant;
+
 public class Main {
     public static void main(String[] args) {
+
+        Instant start = Instant.now();
+        // ...
+        Instant end = Instant.now();
+        Duration duration = Duration.between(start, end);
+        System.out.println(duration.toMillis());
+
         CalculatorFactory.setLogger(true);
         CalculatorFactory.setSecure(true);
         Calculator calculator = CalculatorFactory.getCalculator();
